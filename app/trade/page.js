262565af2e7e0dea8f0ff7c8aa66bfee759907a9
kevthefoo <kevthefoo@gmail.com>
2024-06-21 03:04:01 +1000

    i love j

--- app/trade/page.js
+++ app/trade/page.js
@@ -4,7 +4,7 @@ import { TbArrowsDownUp } from "react-icons/tb";
 import './page.css'
 
 export default function Page() {
-    const [mainValue, setMainValue] = useState(0)
+    const [mainValue, setMainValue] = useState(NaN)
     const [receive, setReceive] = useState(0)
     const [payIsMain, setPayIsMain] = useState(true)
 
@@ -20,7 +20,7 @@ export default function Page() {
 
     function handleSwitch() {
         setPayIsMain(!payIsMain)
-        setMainValue(0)
+        setMainValue(NaN)
         setReceive(0)
     }
 
@@ -34,16 +34,18 @@ export default function Page() {
                     <h1>販賣</h1>
                 </div>
 
-                <div className='pay_input_container flex flex-col border-2 border-gray-500 rounded-lg justify-center items-start px-4 py-2 gap-2'>
+                <div className='relative pay_input_container flex flex-col border-2 border-gray-500 rounded-lg justify-center items-start px-4 py-2 gap-2'>
                     <label htmlFor="" className=''>{payIsMain ? "我要支付" : "我將收到"}</label>
-                    <input className="pay_input bg-transparent outline-none" type="number" id="pay" name="pay" placeholder={0} required value={mainValue} onChange={(e) => handleInputChange(e.target.value)} />
+                    <input className="pay_input bg-transparent outline-none" type="number" id="pay" name="pay" placeholder="" required value={mainValue} onChange={(e) => handleInputChange(e.target.value)} />
+                    <p className="absolute right-2 bottom-3 text-xs">{payIsMain ? "TWD" : "USDT"}</p>
                 </div>
 
                 <div className='border-4 border-white bg-[#facd37] p-2 rounded-full cursor-pointer text-black text-4xl hover:bg-[#e6b832]' onClick={() => handleSwitch()}><i><TbArrowsDownUp /></i></div>
 
-                <div className='pay_input_container flex flex-col border-2 border-gray-500 rounded-lg justify-center items-start px-4 py-2 gap-2'>
+                <div className='relative pay_input_container flex flex-col border-2 border-gray-500 rounded-lg justify-center items-start px-4 py-2 gap-2'>
                     <label htmlFor="" className=''>{payIsMain ? "我將收到" : "我要支付"}</label>
                     <input type="number" id="pay" name="pay" placeholder={0} required className="pay_input bg-transparent outline-none" value={receive} />
+                    <p className="absolute right-2 bottom-3 text-xs">{payIsMain ? "USDT" : "TWD"}</p>
                 </div>
 
                 <a href="" className='text-center border-2 border-white self-center w-1/2 rounded-md p-4 mt-4 hover:bg-[#e6b832] hover:text-black'>送出訂單</a>
